f09bf6bba718e3d8f3aec4277cad9c9a94127c2d
Rudrabose <92892605+Rudrabose@users.noreply.github.com>
2025-04-27 07:25:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "29124227"
+api_hash = "3416803f6b85a63ee12f5976ed557f96"
+bot_token = "7810243090:AAEFjVk1mwunJKT1cHNJm3ELNVCmQbIBGW8"
+auth_users = [7504737201]
+sudo_users = [7504737201]
